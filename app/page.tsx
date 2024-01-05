29bced543e231a88ce4b7c2743f8283eb261432d
aliouuuw <wadealiou00@gmail.com>
2024-01-05 03:34:34 -0500

    Added tremor ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Card, Metric, Text } from '@tremor/react'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-center items-center">
       Puroflow App
+      <Card className="max-w-xs mx-auto">
+        <Text>Sales</Text>
+        <Metric>$ 34,743</Metric>
+      </Card>
     </main>
   )
 }
